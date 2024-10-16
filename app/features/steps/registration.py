832ac4f905767d0c41f846c30b65c6626fd3e8f1
ramiro.santos <ramiro.santos@cardinalsisa.com>
2024-10-15 22:01:12 -0300

    Arregla tests y saca valores default para smtp

--- app/features/steps/registration.py
+++ app/features/steps/registration.py
@@ -19,7 +19,7 @@ def step_when_register_account(context):
     data = {field["field"]: field["value"] for field in context.table}
     response = requests.post(url, json=data)
     context.response = response
-    assert response.status_code == 202, f"Unexpected status code: {response.status_code}, response: {response.text}"
+    assert response.status_code == 201, f"Unexpected status code: {response.status_code}, response: {response.text}"
 
 @then('an account with the email "{email}" is created in the system')
 def step_then_account_created(context, email):
