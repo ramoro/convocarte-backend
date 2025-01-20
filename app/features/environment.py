236de43291d06d2186f943ca51efebd5917f8534
Ramiro Santos <ramiro.santos@cardinalsisa.com>
2025-01-20 13:40:35 -0300

    Actualiza environment.py

--- app/features/environment.py
+++ app/features/environment.py
@@ -36,12 +36,14 @@ def after_scenario(context, scenario):
         # Truncar las tablas en orden correcto para evitar conflictos de claves foráneas
         # No se borra la tabla de usuarios para no estar creando a cada rato el mismo usuario
         # Total siempre se va a necesitar un usuario creado
-        session.execute("TRUNCATE TABLE exposed_roles CASCADE;")
-        session.execute("TRUNCATE TABLE casting_calls CASCADE;")
-        session.execute("TRUNCATE TABLE roles CASCADE;")
-        session.execute("TRUNCATE TABLE projects CASCADE;")
+        session.execute("TRUNCATE TABLE form_template_fields CASCADE;")
         session.execute("TRUNCATE TABLE form_templates CASCADE;")
+        session.execute("TRUNCATE TABLE form_fields CASCADE;")
         session.execute("TRUNCATE TABLE forms CASCADE;")
+        session.execute("TRUNCATE TABLE roles CASCADE;")
+        session.execute("TRUNCATE TABLE casting_calls CASCADE;")
+        session.execute("TRUNCATE TABLE exposed_roles CASCADE;")
+        session.execute("TRUNCATE TABLE projects CASCADE;")
         
         # Confirmar los cambios
         session.commit()
